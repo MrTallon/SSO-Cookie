fd764ec24ca3c2b1bf0f74f215e3b32516d92a3c
MrTallon <352420160@qq.com>
2019-05-02 23:24:10 +0800

    :white_check_mark:常规开发

--- sso-login/src/main/java/com/sso/login/controller/LoginController.java
+++ sso-login/src/main/java/com/sso/login/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.sso.login.controller;
 
+import com.sso.login.pojo.User;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * This is Description
@@ -10,5 +15,28 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class LoginController {
+
+    private static Set<User> dbUsers;
+
+    static {
+        dbUsers.add(new User(0, "占三", "12345"));
+        dbUsers.add(new User(1, "李三", "12345"));
+        dbUsers.add(new User(2, "王三", "12345"));
+        dbUsers.add(new User(3, "赵老爷子", "12345"));
+    }
+
+    public String doLogin(User user, HttpSession session) {
+
+        String target = (String) session.getAttribute("target");
+
+        Optional<User> first = dbUsers.stream().filter(dbUsers -> dbUsers.getUsername().equalsIgnoreCase(user.getUsername()) && dbUsers.getPassword().equals(user.getPassword())).findFirst();
+        if (first.isPresent()) {
+            //保存用户
+
+        } else {
+            //登录失败，返回登陆界面，并且在ession存msg
+        }
+        return "redirect" + target;
+    }
 
 }
--- sso-login/src/main/java/com/sso/login/controller/ViewController.java
+++ sso-login/src/main/java/com/sso/login/controller/ViewController.java
@@ -3,6 +3,10 @@ package com.sso.login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * 页面跳转逻辑
@@ -16,10 +20,17 @@ public class ViewController {
 
     /**
      * 跳转到登录页面
+     *
      * @return
      */
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(required = false, defaultValue = "") String target, HttpSession session) {
+
+        if (StringUtils.isEmpty(target)) {
+            target = "http://www.codeshop.com";
+        }
+        // 地址合法性校验
+        session.setAttribute("target", target);
         return "login";
     }
 
